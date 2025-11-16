7f90be6c051d182c40796f9518ee5f2b584ab3f4
Christian Luis C. Fernandez <chrisluisfern1@gmail.com>
2025-11-16 12:38:23 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -150,7 +150,9 @@ with tabs[1]:
     st.subheader("🖼 Gallery")
 
     st.markdown("### 📄 My CV")
-    st.markdown("[Click here to view my CV on Google Docs](https://docs.google.com/document/d/1eac6Cdl-W84E6a_jSmyd88HoeMaatVd1/edit?usp=sharing)")
+    st.markdown(
+        "[Click here to view my CV on Google Docs](https://docs.google.com/document/d/1eac6Cdl-W84E6a_jSmyd88HoeMaatVd1/edit?usp=sharing)"
+    )
 
 
     st.markdown("---")
